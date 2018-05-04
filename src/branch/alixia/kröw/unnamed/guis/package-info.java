d900f593eb794d56260a5e526b9af828e74d0739
Zeale <Zeale@dusttoash.org>
2018-05-03 22:09:26 -0500

    Added some package info!

---
+++ src/branch/alixia/kröw/unnamed/guis/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Internal GUI package for {@link branch.alixia.unnamed.Unnamed}. This package
+ * provides either internal API classes or internal, full windows for Unnamed.
+ * 
+ * @author Zeale
+ *
+ */
+package branch.alixia.kröw.unnamed.guis;
---
+++ src/branch/alixia/kröw/unnamed/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * This package is the root package of (most) of
+ * {@link branch.alixia.unnamed.Unnamed}'s internal/private code. Feel free to
+ * like, explore this and stuff.<br>
+ * <br>
+ * All public APIs are stored in other packages in
+ * <code>branch.alixia.unnamed</code>. Everything inside
+ * <code>branch.alixia.kröw</code> is private.<br>
+ * <br>
+ * This program isn't an API, it's a program, so I can't really determine how to
+ * style the packaging off of the Java Library's source code like I can with
+ * variable names or other convention related things. I just kinda shoved all
+ * private things into the <code>kröw</code> package.
+ * 
+ * @author Zeale
+ *
+ */
+package branch.alixia.kröw.unnamed;
