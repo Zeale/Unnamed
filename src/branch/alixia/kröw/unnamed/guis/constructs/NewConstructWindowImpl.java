cecd97261ce062fc63906944fb0c5bf6da4f869e
Zeale <Zeale@dusttoash.org>
2018-05-08 16:42:57 -0500

    Updated more options in new construct window

--- src/branch/alixia/kröw/unnamed/guis/constructs/NewConstructWindowImpl.java
+++ src/branch/alixia/kröw/unnamed/guis/constructs/NewConstructWindowImpl.java
@@ -9,7 +9,6 @@ import branch.alixia.kröw.unnamed.tools.FXTools;
 import branch.alixia.msapi.Construct;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -18,6 +17,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.TilePane;
@@ -59,8 +59,7 @@ class NewConstructWindowImpl extends UWindowBase {
 		/*
 		 * Pane Layout
 		 */
-		setMinSize(800, 600);
-		setPrefSize(800, 600);
+		wrapper.setPrefSize(800, 600);
 		setCenter(wrapper);
 		setBorder(FXTools.getBorderFromColor(ITEM_BORDER_COLOR.interpolate(DEFAULT_WINDOW_BACKGROUND_COLOR, 0.5)));
 
@@ -80,7 +79,6 @@ class NewConstructWindowImpl extends UWindowBase {
 		 * Menu Bar Setup
 		 */
 		getMenuBar().new MenuItem((Node) cancel);
-
 		FXTools.applyColorwheelTransition(cancel);
 		cancel.setFont(Font.font(null, FontWeight.BOLD, -1));
 		cancel.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -128,15 +126,18 @@ class NewConstructWindowImpl extends UWindowBase {
 
 	}
 
-	private void showExtraProperties() {
-		setPrefSize(1400, 800);
-		setCenter(optionsScrollWrapper);
+	private void showNormalProperties() {
+		stage.setWidth(800);
+		stage.setHeight(600);
+		stage.centerOnScreen();
+		setCenter(wrapper);
 	}
 
 	/*
 	 * Extra Properties Window
 	 */
 
+	private final ToggleGroup minorMajorGroup = new ToggleGroup(), pMTGroup = new ToggleGroup();
 	private final RadioButton minor = new RadioButton("Minor"), major = new RadioButton("Major"),
 			perMech = new RadioButton("Personality/Mechanical"), properTitan = new RadioButton("Proper Titan"),
 			elevatedTitan = new RadioButton("Elevated Titan"), honoraryTitan = new RadioButton("Honorary Titan");
@@ -152,23 +153,36 @@ class NewConstructWindowImpl extends UWindowBase {
 
 	{
 
+		AnchorPane.setTopAnchor(optionsContent, 110d);
+		AnchorPane.setRightAnchor(optionsContent, 0d);
+		AnchorPane.setLeftAnchor(optionsContent, 0d);
+		AnchorPane.setBottomAnchor(optionsContent, 0d);
+
+		minorMajorGroup.getToggles().addAll(minor, major);
+		pMTGroup.getToggles().addAll(perMech, properTitan, elevatedTitan, honoraryTitan);
+
+		FXTools.styleBasicInput(minor, major, perMech, properTitan, elevatedTitan, honoraryTitan);
+
 		AnchorPane.setTopAnchor(back, 35d);
 		AnchorPane.setLeftAnchor(back, 35d);
 
 		classWrapper.setHgap(15);
 		classWrapper.setVgap(5);
 		classWrapper.setAlignment(Pos.CENTER);
-		classWrapper.setPadding(new Insets(0, 25, 0, 25));
 
 		FXTools.styleBasicInput(back);
 		back.setOnAction(event -> showNormalProperties());
 
 		FXTools.setDefaultBackground(optionsScrollWrapper);
+		optionsScrollWrapper.setFitToWidth(true);
+
 	}
 
-	private void showNormalProperties() {
-		setPrefSize(800, 600);
-		setCenter(wrapper);
+	private void showExtraProperties() {
+		stage.setWidth(1400);
+		stage.setHeight(800);
+		stage.centerOnScreen();
+		setCenter(optionsScrollWrapper);
 	}
 
 }
