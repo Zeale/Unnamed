931dcf58d854e55e428562a04b81bef0b6b4326b
Zeale <Zeale@dusttoash.org>
2018-05-16 16:51:03 -0500

    Window can now be resized

--- src/branch/alixia/unnamed/gui/UWindowBase.java
+++ src/branch/alixia/unnamed/gui/UWindowBase.java
@@ -2,14 +2,18 @@ package branch.alixia.unnamed.gui;
 
 import branch.alixia.kröw.unnamed.DefaultMenuBar;
 import branch.alixia.kröw.unnamed.tools.FXTools;
+import branch.alixia.kröw.unnamed.tools.Resizable;
+import branch.alixia.kröw.unnamed.tools.ResizeOperator;
 import branch.alixia.unnamed.MenuBar;
 import branch.alixia.unnamed.Unnamed;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Window;
 
-public class UWindowBase extends BorderPane {
+public class UWindowBase extends BorderPane implements Resizable {
 
 	public static final Color DEFAULT_WINDOW_BACKGROUND_COLOR = Unnamed.DEFAULT_WINDOW_COLOR;
 	public static final Color ITEM_BORDER_COLOR = Unnamed.ITEM_BORDER_COLOR;
@@ -21,6 +25,9 @@ public class UWindowBase extends BorderPane {
 	{
 		topProperty().bind(menuBar);
 	}
+
+	private final ResizeOperator resizer = new ResizeOperator(this, this);
+	private final BooleanProperty resizeEnabled = resizer.enabledProperty();
 
 	protected static final Color DEFAULT_BORDER_COLOR = DEFAULT_WINDOW_BACKGROUND_COLOR.darker().darker();
 
@@ -39,6 +46,42 @@ public class UWindowBase extends BorderPane {
 
 	protected final void setMenuBar(final MenuBar menuBar) {
 		this.menuBarProperty().set(menuBar);
+	}
+
+	@Override
+	public void expandHor(double amount) {
+		Window window = getScene().getWindow();
+		window.setWidth(window.getWidth() + amount);
+	}
+
+	@Override
+	public void expandVer(double amount) {
+		Window window = getScene().getWindow();
+		window.setHeight(window.getHeight() + amount);
+	}
+
+	@Override
+	public void moveX(double amount) {
+		Window window = getScene().getWindow();
+		window.setX(amount + window.getX());
+	}
+
+	@Override
+	public void moveY(double amount) {
+		Window window = getScene().getWindow();
+		window.setY(amount + window.getY());
+	}
+
+	public final BooleanProperty resizeEnabledProperty() {
+		return this.resizeEnabled;
+	}
+
+	public final boolean isResizeEnabled() {
+		return this.resizeEnabledProperty().get();
+	}
+
+	public final void setResizeEnabled(final boolean resizeEnabled) {
+		this.resizeEnabledProperty().set(resizeEnabled);
 	}
 
 }
